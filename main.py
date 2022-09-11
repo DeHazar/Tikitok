1ab26e794366a349871bd2af708adb3724d11922
Гараев Денис Нагимович <DGaraev@alfabank.ru>
2022-09-11 16:54:33 +0500

    DONE

--- main.py
+++ main.py
@@ -25,35 +25,35 @@ api = int(os.environ.get('API_KEY'))
 hash = os.environ.get('API_HASH')
 chnnl = os.environ.get('CHANNEL_URL')
 BOT_URL = os.environ.get('BOT_URL')
-app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=workers)
-
+app = Client("JayBee", bot_token=bot_token, api_id=api,
+             api_hash=hash, workers=workers)
 
 
 @app.on_message(filters.command('start'))
 def start(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
+    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl), InlineKeyboardButton(
+        'Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
     reply_markup = InlineKeyboardMarkup(kb)
     app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download TikTok video without Watermark.\n\n"
-                          "__**Developer :**__ __@JayBeeDev__\n"
-                          "__**Language :**__ __Python__\n"
-                          "__**Framework :**__ __🔥 Pyrogram__",
+                     "__**Developer :**__ __@JayBeeDev__\n"
+                     "__**Language :**__ __Python__\n"
+                     "__**Framework :**__ __🔥 Pyrogram__",
                      parse_mode='md',
                      reply_markup=reply_markup)
-
-
 
 
 @app.on_message(filters.command('help'))
 def help(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
+    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl), InlineKeyboardButton(
+        'Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
     reply_markup = InlineKeyboardMarkup(kb)
     app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download any TikTok video from a given link.\n\n"
-                                            "__Send me a TikTok video link__",
+                     "__Send me a TikTok video link__",
                      parse_mode='md',
                      reply_markup=reply_markup)
 
 
-@app.on_message((filters.regex("http://")|filters.regex("https://")) & (filters.regex('tiktok')|filters.regex('douyin')))
+@app.on_message((filters.regex("http://") | filters.regex("https://")) & (filters.regex('tiktok') | filters.regex('douyin')))
 def tiktok_dl(client, message):
     a = app.send_message(chat_id=message.chat.id,
                          text='__Downloading File to the Server__',
@@ -61,20 +61,16 @@ def tiktok_dl(client, message):
     link = re.findall(r'\bhttps?://.*[(tiktok|douyin)]\S+', message.text)[0]
     link = link.split("?")[0]
 
-
-
-    
     params = {
-      "link": link
+        "link": link
     }
     headers = {
-      'x-rapidapi-host': "tiktok-info.p.rapidapi.com",
-      'x-rapidapi-key': "fc4bb079c4msh549914e2b6707a8p1ed812jsn6776155b43a2"
+        'x-rapidapi-host': "tiktok-info.p.rapidapi.com",
+        'x-rapidapi-key': "fc4bb079c4msh549914e2b6707a8p1ed812jsn6776155b43a2"
     }
-    
-    ### Get your Free TikTok API from https://rapidapi.com/TerminalWarlord/api/tiktok-info/
-    #Using the default one can stop working any moment 
-    
+
+    # Get your Free TikTok API from https://rapidapi.com/TerminalWarlord/api/tiktok-info/
+    # Using the default one can stop working any moment
     api = f"https://tiktok-info.p.rapidapi.com/dl/"
     request = requests.get(api, params=params, headers=headers)
     r = request.json()['videoLinks']['download']
@@ -82,51 +78,55 @@ def tiktok_dl(client, message):
     filename = str(int(time.time()))+'.mp4'
     size = int(request.headers['content-length'])
     total_size = "{:.2f}".format(int(size) / 1048576)
-    try:
-        os.mkdir(directory)
-    except:
-        pass
-    with requests.get(r, timeout=(50, 10000), stream=True) as r:
-        r.raise_for_status()
-        with open(f'./{directory}/{filename}', 'wb') as f:
-            chunk_size = 1048576
-            dl = 0
-            show = 1
-            for chunk in r.iter_content(chunk_size=chunk_size):
-                f.write(chunk)
-                dl = dl + chunk_size
-                percent = round(dl * 100 / size)
-                if percent > 100:
-                    percent = 100
-                if show == 1:
-                    try:
-                        a.edit(f'__**URL :**__ __{message.text}__\n'
-                               f'__**Total Size :**__ __{total_size} MB__\n'
-                               f'__**Downloaded :**__ __{percent}%__\n',
-                               disable_web_preview=False)
-                    except:
-                        pass
-                    if percent == 100:
-                        show = 0
+    app.send_message(chat_id=message.from_user.id, text=f"Ссылка на видео:\n\n"
+                     r,
+                     parse_mode='md',
+                     reply_markup=reply_markup)
+    # try:
+    #     os.mkdir(directory)
+    # except:
+    #     pass
+    # with requests.get(r, timeout=(50, 10000), stream=True) as r:
+    #     r.raise_for_status()
+    #     with open(f'./{directory}/{filename}', 'wb') as f:
+    #         chunk_size = 1048576
+    #         dl = 0
+    #         show = 1
+    #         for chunk in r.iter_content(chunk_size=chunk_size):
+    #             f.write(chunk)
+    #             dl = dl + chunk_size
+    #             percent = round(dl * 100 / size)
+    #             if percent > 100:
+    #                 percent = 100
+    #             if show == 1:
+    #                 try:
+    #                     a.edit(f'__**URL :**__ __{message.text}__\n'
+    #                            f'__**Total Size :**__ __{total_size} MB__\n'
+    #                            f'__**Downloaded :**__ __{percent}%__\n',
+    #                            disable_web_preview=False)
+    #                 except:
+    #                     pass
+    #                 if percent == 100:
+    #                     show = 0
 
-        a.edit(f'__Downloaded to the server!\n'
-               f'Uploading to Telegram Now ⏳__')
-        start = time.time()
-        title = filename
-        app.send_document(chat_id=message.chat.id,
-                          document=f"./{directory}/{filename}",
-                          caption=f"**File :** __{filename}__\n"
-                          f"**Size :** __{total_size} MB__\n\n"
-                          f"__Uploaded by @{BOT_URL}__",
-                          file_name=f"{directory}",
-                          parse_mode='md',
-                          progress=progress,
-                          progress_args=(a, start, title))
-        a.delete()
-        try:
-            shutil.rmtree(directory)
-        except:
-            pass
+    #     a.edit(f'__Downloaded to the server!\n'
+    #            f'Uploading to Telegram Now ⏳__')
+    #     start = time.time()
+    #     title = filename
+    #     app.send_document(chat_id=message.chat.id,
+    #                       document=f"./{directory}/{filename}",
+    #                       caption=f"**File :** __{filename}__\n"
+    #                       f"**Size :** __{total_size} MB__\n\n"
+    #                       f"__Uploaded by @{BOT_URL}__",
+    #                       file_name=f"{directory}",
+    #                       parse_mode='md',
+    #                       progress=progress,
+    #                       progress_args=(a, start, title))
+    #     a.delete()
+    #     try:
+    #         shutil.rmtree(directory)
+    #     except:
+    #         pass
 
 
 app.run()
